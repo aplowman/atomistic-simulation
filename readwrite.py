154b62e50615e59942b5c5e82c49d800c8368698
APlowman <adam.jp@live.co.uk>
2017-06-29 18:45:22 +0100

    Format readwrite.py

--- readwrite.py
+++ readwrite.py
@@ -2,15 +2,15 @@ import os
 import re
 import pickle
 
-def find_files_in_dir(dir_path, match_regex, recursive=False):
 
+def find_files_in_dir(dir_path, match_regex, recursive=False):
     """ 
     Return paths (relative to `dir_path`) of file names in `dir_path` which 
     match the regular expression given by `match_regex`. If `recursive` is
     True, search all subdirectories.
 
     Returns a list of strings.
-    
+
     """
 
     matched_files = []
@@ -18,7 +18,7 @@ def find_files_in_dir(dir_path, match_regex, recursive=False):
     for idx, (root, _, files) in enumerate(os.walk(dir_path)):
 
         for f in files:
-            
+
             if re.search(match_regex, f):
 
                 if idx == 0:
@@ -30,21 +30,21 @@ def find_files_in_dir(dir_path, match_regex, recursive=False):
                 matched_files.append(matched_path)
 
         if not recursive:
-            break        
+            break
 
     return matched_files
 
-def write_pickle(obj, file_path):
 
+def write_pickle(obj, file_path):
     """ Write `obj` to a pickle file at `file_path`. """
 
-    with open(file_path,'wb') as f:
+    with open(file_path, 'wb') as f:
         pickle.dump(obj, f)
 
-def read_pickle(file_path):
 
+def read_pickle(file_path):
     """ Return object in pickle file given by `file_path`. """
 
-    with open(file_path,'rb') as f:
+    with open(file_path, 'rb') as f:
         obj = pickle.load(f)
-    return obj    
+    return obj

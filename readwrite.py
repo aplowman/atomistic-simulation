9cbfb8347dac3d384f4473f88483fb6ec247a181
APlowman <adam.jp@live.co.uk>
2017-07-31 23:16:06 +0100

    Add find_files_in_dir_glob for Unix-style pattern matching

--- readwrite.py
+++ readwrite.py
@@ -40,6 +40,38 @@ def find_files_in_dir(dir_path, match_regex, recursive=False):
     return matched_files
 
 
+def find_files_in_dir_glob(dir_path, match_glob, recursive=False):
+    """ 
+    Return paths (relative to `dir_path`) of file names in `dir_path` which 
+    match glob pattern `match_glob`. If `recursive` is True, search all
+    subdirectories.
+
+    Returns a list of strings.
+
+    """
+
+    matched_files = []
+
+    for idx, (root, _, files) in enumerate(os.walk(dir_path)):
+
+        for f in files:
+
+            if fnmatch.fnmatch(f, match_glob):
+
+                if idx == 0:
+                    matched_path = f
+
+                else:
+                    matched_path = os.path.join(root.split(os.path.sep)[-1], f)
+
+                matched_files.append(matched_path)
+
+        if not recursive:
+            break
+
+    return matched_files
+
+
 def make_int_dir(path, zero_pad=2):
     """
     Find directories in a path whose names can be parsed as integers and make a

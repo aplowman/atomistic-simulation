45643599b5e97ae77b5aea8996c3832b6916211f
APlowman <adam.jp@live.co.uk>
2017-08-23 12:05:43 +0100

    Extensive reworking

--- harvest.py
+++ harvest.py
@@ -8,7 +8,7 @@ import shutil
 import json
 import copy
 from set_up.opt_test_res import RES_OPT
-from postprocess import SINGLE_COMPUTES, MULTI_COMPUTES, compute_gb_energy, dict_from_list
+from postprocess import SINGLE_COMPUTES, MULTICOMPUTE_LOOKUP, compute_gb_energy, dict_from_list, get_required_defn
 import plotting
 import vectors
 
@@ -73,7 +73,7 @@ def read_results(sid, skip_idx=None, query_all=False):
     write_pickle(sims, pick_path)
 
 
-def collate_results(res_opt):
+def collate_results(res_opt, debug=False):
     """
     Save a JSON file containing the results of one of more simulation series.
 
@@ -84,6 +84,10 @@ def collate_results(res_opt):
 
     rs_date, rs_num = utils.get_date_time_stamp(split=True)
     rs_id = rs_date + '_' + rs_num
+
+    # TEMP:
+    if debug:
+        rs_id = '0000-00-00-0000_00000'
 
     def get_series_vals(series_id):
         srs_vals = []
@@ -101,56 +105,98 @@ def collate_results(res_opt):
             srs_paths.append('_'.join([i['path'] for i in srs_id_lst]))
         return srs_paths
 
-    # Find required parameters/results neccessary for multi-computes
-    additional_cmpt = []
-    for cmpt in res_opt['computes']:
-        cmpt_name = cmpt['name']
-        cmpt_defn = MULTI_COMPUTES.get(cmpt_name)
-        if cmpt_defn is not None:
-            requires = cmpt_defn['requires']
-            req_com = requires['computes']
-            req_par = requires['parameters']
-            req_res = requires['results']
-            additional_cmpt.extend(req_com)
-
-            # Very hacky, OK for now:
-            for r_idx in range(len(req_res)):
-                r = req_res[r_idx]
-                if not isinstance(r, dict):
-                    # Assume a function
-                    req_res[r_idx] = r(cmpt['energy_src'])
-
-            # TODO: Need to check that parameter/result is not already defined here
-            res_opt['parameters'].extend(req_par)
-            res_opt['results'].extend(req_res)
-
-            print('req_res: {}'.format(req_res))
-
-    res_opt['computes'].extend(additional_cmpt)
-
+    variables = []
     computes = []
-    for c_idx, c in enumerate(res_opt['computes']):
-        c_copy = copy.deepcopy(c)
-        c_copy.update({'vals': []})
-        computes.append(c_copy)
+    computes_idx = []
+    additional_vars = []
+    ids = []
+    required_keys = ['name', 'type', 'id', ]
+    std_keys = required_keys + ['display_name', 'idx', 'vals', ]
+    allowed_types = ['result', 'parameter', 'compute', 'common_series_info', ]
+    for vr_idx, vr in enumerate(res_opt['variables']):
 
-    parameters = []
-    for p_idx, p in enumerate(res_opt['parameters']):
-        p_copy = copy.deepcopy(p)
-        p_copy.update({'vals': []})
-        parameters.append(p_copy)
+        # Check type is allowed:
+        if vr['type'] not in allowed_types:
+            raise ValueError('"{}" is not an allowed `type`: {}'.format(
+                vr['type'], allowed_types))
 
-    results = []
-    for r_idx, r in enumerate(res_opt['results']):
-        r_copy = copy.deepcopy(r)
-        r_copy.update({'vals': []})
-        results.append(r_copy)
+        # Check all required keys are given:
+        for rk in required_keys:
+            if rk not in vr:
+                rk_error = 'Variable #{} must have key: {}'.format(vr_idx, rk)
+                raise ValueError(rk_error)
 
-    com_srs = []
-    for cs_idx, cs in enumerate(res_opt['common_series_info']):
-        cs_copy = copy.deepcopy(cs)
-        cs_copy.update({'vals': []})
-        com_srs.append(cs_copy)
+        # Check `id` is not repeated
+        if vr['id'] not in ids:
+            ids.append(vr['id'])
+        else:
+            raise ValueError('Variable `id` must be unique.')
+
+        vr_copy = copy.deepcopy(vr)
+        vr_copy.update({'vals': []})
+
+        if vr['type'] == 'compute':
+            computes_idx.append(vr_idx)
+            computes.append(vr_copy)
+            compute_kwargs = {k: v for k, v in vr.items() if k not in std_keys}
+            additional_vars += get_required_defn(vr['name'], **compute_kwargs)
+        else:
+            variables.append(vr_copy)
+
+    # Merge additional variables due to computes to variables list
+    # If the variable `name`, `type` and `idx` all match, combine, keeping
+    # user-specified variable `id`, otherwise add as an additional variable.
+    # If an `id` is already in use, append a number to the `id`.
+    for advar in additional_vars:
+
+        # print('considering advar: \n{}\n'.format(advar))
+
+        # Find if the variable already exists (i.e. if it was user-specified):
+        conditions = {
+            'name': advar['name'],
+            'type': advar['type'],
+        }
+        advar_idx = advar.get('idx')
+        if advar_idx is None:
+            false_keys = ['idx']
+        else:
+            false_keys = None
+            conditions.update({'idx': advar_idx})
+
+        if advar['type'] == 'compute':
+            append_target = computes
+        else:
+            append_target = variables
+
+        # print('append_target is currently: \n\n{}'.format(append_target))
+
+        m_idx, _ = dict_from_list(append_target, conditions,
+                                  false_keys=false_keys,
+                                  ret_index=True)
+
+        if m_idx is not None:
+            merged_var = {**advar, **append_target[m_idx]}
+            append_target[m_idx] = merged_var
+
+        else:
+            # Check id does not exist, change if necessary
+            trial_id = advar['id']
+            id_match = dict_from_list(append_target, {'id': trial_id})
+            count = 1
+            while id_match is not None:
+                trial_id += '_{:d}'.format(count)
+                id_match = dict_from_list(append_target, {'id': trial_id})
+                count += 1
+
+            advar['id'] = trial_id
+            append_target.append(advar)
+
+    print('variables 2: {}'.format(format_list(variables)))
+    print('computes: {}'.format(format_list(computes)))
+
+    common_series_info = []
+    for csi_idx, csi in enumerate(res_opt['common_series_info']):
+        common_series_info
 
     out = {
         'session_id': [],
@@ -158,13 +204,13 @@ def collate_results(res_opt):
         'series_name': [],
         'series_id_val': [],
         'series_id_path': [],
-        'computes': computes,
-        'parameters': parameters,
-        'results': results,
-        'common_series_info': com_srs,
+        'variables': variables + computes,
     }
 
-    # Loop through series IDs:
+    print('variables: '.format(out['variables']))
+
+    # Loop through series IDs and sims to append values to `result` and
+    # `parameter` variable types:
     sids = res_opt['sid']
     for sid in sids:
 
@@ -173,6 +219,23 @@ def collate_results(res_opt):
         pick = read_pickle(pick_path)
         sims = pick['all_sims']
         base_opt = pick['base_options']
+        csi = pick['common_series_info']
+
+        for vr_idx, vr in enumerate(out['variables']):
+
+            vr_name = vr['name']
+            vr_type = vr['type']
+
+            if vr_type == 'common_series_info':
+
+                val = csi[vr_name]
+                all_sub_idx = vr.get('idx')
+                if all_sub_idx is not None:
+                    for sub_idx in all_sub_idx:
+                        val = val[sub_idx]
+                if isinstance(val, np.ndarray):
+                    val = val.tolist()
+                out['variables'][vr_idx]['vals'] = val
 
         # Get a flat list of series names for this sim series
         srs_name = []
@@ -190,64 +253,49 @@ def collate_results(res_opt):
             out['series_id_path'].append(get_series_paths(srs_id))
             out['series_id_val'].append(get_series_vals(srs_id))
 
-            sm_results = sm.results
+            for vr_idx, vr in enumerate(out['variables']):
 
-            # Execute single-calc computes here:
-            # Single computes are defined as functions which take the output from a single sim
-            for c_idx, c in enumerate(out['computes']):
-                c_name = c['name']
-                c_props = SINGLE_COMPUTES.get(c_name)
-                if c_props is not None:
-                    v = c_props[0](sm, *c_props[1:])
-                    all_sub_idx = c.get('idx')
-                    if all_sub_idx is not None:
-                        for sub_idx in all_sub_idx:
-                            v = v[sub_idx]
-                    out['computes'][c_idx]['vals'].append(v)
+                vr_name = vr['name']
+                vr_type = vr['type']
 
-            for r_idx, r in enumerate(out['results']):
-
-                v = sm.results[r['name']]
-                all_sub_idx = r.get('idx')
+                val = None
+                if vr_type == 'result':
+                    val = sm.results[vr_name]
+                elif vr_type == 'parameter':
+                    val = sm.options[vr_name]
+                elif vr_type == 'compute':
+                    cmpt_props = SINGLE_COMPUTES.get(vr_name)
+                    if cmpt_props is not None:
+                        val = cmpt_props[0](sm, *cmpt_props[1:])
+                    else:
+                        continue
+                else:
+                    continue
+                all_sub_idx = vr.get('idx')
                 if all_sub_idx is not None:
                     for sub_idx in all_sub_idx:
-                        v = v[sub_idx]
-                        if isinstance(v, np.ndarray):
-                            v = v.tolist()
+                        val = val[sub_idx]
+                if isinstance(val, np.ndarray):
+                    val = val.tolist()
 
-                out['results'][r_idx]['vals'].append(v)
+                print('val ({}): {} ({})'.format(vr_name, val, type(val)))
 
-            for p_idx, p in enumerate(out['parameters']):
+                out['variables'][vr_idx]['vals'].append(val)
 
-                v = sm.options[p['name']]
-                all_sub_idx = p.get('idx')
-                if all_sub_idx is not None:
-                    for sub_idx in all_sub_idx:
-                        v = v[sub_idx]
-                        if isinstance(v, np.ndarray):
-                            v = v.tolist()
+    print('out[vars]: \n{}\n'.format(format_list(out['variables'])))
 
-                out['parameters'][p_idx]['vals'].append(v)
+    # Now calculate multi `compute`s:
+    for vr_idx, vr in enumerate(out['variables']):
 
-        for cs_idx, cs in enumerate(out['common_series_info']):
+        if vr['type'] != 'compute':
+            continue
 
-            v = pick['common_series_info'][cs['name']]
-            print('v: \n{}\n'.format(v))
-            all_sub_idx = cs.get('idx')
-            if all_sub_idx is not None:
-                for sub_idx in all_sub_idx:
-                    v = v[sub_idx]
-                    if isinstance(v, np.ndarray):
-                        v = v.tolist()
-
-            out['common_series_info'][cs_idx]['vals'].append(v)
-
-    # Execute computes which require more than one simulation
-    # GB compute:
-    for c_idx, c in enumerate(out['computes']):
-        c_props = MULTI_COMPUTES.get(c['name'])
-        if c_props is not None:
-            c_props['func'](out)
+        cmpt_name = vr['name']
+        cmpt_props = SINGLE_COMPUTES.get(cmpt_name)
+        if cmpt_props is None:
+            func = MULTICOMPUTE_LOOKUP[cmpt_name]
+            args = {k: v for k, v in vr.items() if k not in std_keys}
+            func(out, **args)
 
     # Save the JSON file in the results directory of the first listed SID
     res_dir = os.path.join(RES_PATH, rs_id)
@@ -265,13 +313,40 @@ def collate_results(res_opt):
     # Plots
     for pl in res_opt['plots']:
 
-        x_name = pl['x']['name']
-        y_name = pl['y']['name']
+        x = pl['x']
+        y = pl['y']
 
-        # # Find this x data within parameters, results or computes:
-        all_d = out['parameters'] + out['results'] + out['computes']
-        x_data = dict_from_list(all_d, name=x_name)['vals']
-        y_data = dict_from_list(all_d, name=y_name)['vals']
+        x_id = x['id']
+        y_id = y['id']
+        x_label = x['label']
+        y_label = y['label']
+
+        z = pl.get('z')
+        if z is not None:
+            z_id = z['id']
+            z_label = z['label']
+
+        x_data = dict_from_list(out['variables'], {'id': x_id})['vals']
+        all_x_subidx = pl['x'].get('idx')
+        if all_x_subidx is not None:
+            for x_subidx in all_x_subidx:
+                x_data = x_data[x_subidx]
+        print('x_data: \n{}\n'.format(x_data))
+
+        y_data = dict_from_list(out['variables'], {'id': y_id})['vals']
+        all_y_subidx = pl['y'].get('idx')
+        if all_y_subidx is not None:
+            for y_subidx in all_y_subidx:
+                y_data = y_data[y_subidx]
+        print('y_data: \n{}\n'.format(y_data))
+
+        if z is not None:
+            z_data = dict_from_list(out['variables'], {'id': z_id})['vals']
+            all_z_subidx = z.get('idx')
+            if all_z_subidx is not None:
+                for z_subidx in all_z_subidx:
+                    z_data = z_data[z_subidx]
+            print('z_data: \n{}\n'.format(z_data))
 
         pl_srs_id = pl.get('series_id')
         if pl_srs_id is not None:
@@ -279,30 +354,54 @@ def collate_results(res_opt):
             for pid in pl_srs_id:
                 pl_srs.append(dict_from_list(all_d, id=pid)['vals'])
 
-        # print('pl_srs: {}'.format(pl_srs))
+        plot_name = '{}_{}'.format(x_label, y_label)
+        if z is not None:
+            plot_name += '_{}'.format(z_label)
+        plot_path = os.path.join(res_dir, plot_name)
+        print('plt_path: {}'.format(plot_path))
 
-        plot_path = os.path.join(res_dir, '{}_{}'.format(x_name, y_name))
-
-        # Identify unique traces by series:
-        traces = {}
-        for x_idx, x in enumerate(x_data):
-            trace_id = utils.transpose_list(pl_srs)[x_idx]
-            trace_id_str = ' '.join([str(t) for t in trace_id])
-            if trace_id_str in traces:
-                traces[trace_id_str]['x'].append(x)
-                traces[trace_id_str]['y'].append(y_data[x_idx])
-            else:
-                traces.update({
-                    trace_id_str: {
-                        'x': [x],
-                        'y': [y_data[x_idx]]
+        if pl_srs_id is None:
+            if z is None:
+                traces = {
+                    y_label: {
+                        'x': x_data,
+                        'y': y_data,
                     }
-                })
+                }
+            else:
+                traces = {
+                    z_label: {
+                        'x': x_data,
+                        'y': y_data,
+                        'z': z_data,
+                    }
+                }
+        else:
+            # Identify unique traces by series:
+            traces = {}
+            for x_idx, x in enumerate(x_data):
+
+                if isinstance(pl_srs[0], list):
+                    pl_srs = utils.transpose_list(pl_srs)
+                trace_id = pl_srs[x_idx]
+                trace_id_str = ' '.join([str(t) for t in trace_id])
+                if trace_id_str in traces:
+                    traces[trace_id_str]['x'].append(x)
+                    traces[trace_id_str]['y'].append(y_data[x_idx])
+                else:
+                    traces.update({
+                        trace_id_str: {
+                            'x': [x],
+                            'y': [y_data[x_idx]]
+                        }
+                    })
 
         for k, v in traces.items():
             utils.trim_common_nones(v['x'], v['y'])
 
         all_traces = [traces]
+        print('all_traces: {}'.format(all_traces))
+
         if pl.get('plot_sequential_diff') is True:
 
             # Add a subplot showing difference between adjacent y values
@@ -311,7 +410,7 @@ def collate_results(res_opt):
                 all_traces[1][k]['x'] = v['x'][1:]
                 all_traces[1][k]['y'] = np.ediff1d(v['y'])
 
-        # print('trace: {}'.format(traces))
+        # print('traces: {}'.format(traces))
 
         if pl['lib'] == 'plotly':
             save_args = {
@@ -322,7 +421,10 @@ def collate_results(res_opt):
 
         elif pl['lib'] == 'mpl':
             plot_path += '.' + pl['fmt']
-            plotting.basic_plot_mpl(traces, plot_path)
+            if z is None:
+                plotting.basic_plot_mpl(traces, plot_path)
+            else:
+                plotting.contour_plot_mpl(traces, plot_path)
 
         elif pl['lib'] == 'bokeh':
             plot_path += '.html'
@@ -337,11 +439,11 @@ def main():
     if skip_idx is None or len(skip_idx) == 0:
         skip_idx = [[] for _ in range(len(sids))]
 
-    for s_idx, s in enumerate(sids):
-        read_results(s, skip_idx=skip_idx[s_idx])
+    # for s_idx, s in enumerate(sids):
+    #     read_results(s, skip_idx=skip_idx[s_idx])
 
     # Compute additional properties
-    collate_results(RES_OPT)
+    collate_results(RES_OPT, debug=True)
 
 
 if __name__ == '__main__':

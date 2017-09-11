f348a2b18ae634b6095ee704a1f3646cd1cf18da
maria-yankova <maria.yankova@postgrad.manchester.ac.uk>
2017-09-11 17:04:49 +0100

    Typos

--- gbhelper.py
+++ gbhelper.py
@@ -4,6 +4,8 @@ import simsio
 from functools import reduce
 
 # Construct grain boundaries functions
+
+
 def construct_180_001_mZrO2(cellfile, repeats=[3, 1, 1], bound_vac=0.2,
                             transls=[0.0, 0.0], term_plns=['100', '100']):
     """
@@ -55,7 +57,7 @@ def construct_180_001_mZrO2(cellfile, repeats=[3, 1, 1], bound_vac=0.2,
 
     Notes
     -----
-            'supercell': supercell.T,
+        'supercell': supercell.T,
         'pos_gb_a': pos_gb_a,
         'pos_gb_b': pos_gb_b,
         'atom_sites' : np.concatenate((pos_gb_a, pos_gb_b)).T,
@@ -85,7 +87,7 @@ def construct_180_001_mZrO2(cellfile, repeats=[3, 1, 1], bound_vac=0.2,
 
     # Read in data for input structure and set up cell, pos_f, species, natoms
     lat_data = simsio.read_cell_file(cellfile)
-    
+
     cell_vecs = lat_data['cell_vecs']
 
     cell = align_mon_cart(cell_vecs, axis='c')  # Align c with z
@@ -93,8 +95,7 @@ def construct_180_001_mZrO2(cellfile, repeats=[3, 1, 1], bound_vac=0.2,
     species_key = get_species_key_idx(lat_data['motif']['species'])[0]
     species = get_species_key_idx(lat_data['motif']['species'])[1]
     natoms = len(species)
-    print(pos_f)
-    
+
     # Translate atoms based on termination planes for grains `a` and `b`.
     pos_f_a = np.zeros((natoms, 3))
     pos_f_b = np.zeros((natoms, 3))
@@ -140,33 +141,31 @@ def construct_180_001_mZrO2(cellfile, repeats=[3, 1, 1], bound_vac=0.2,
     supercell[0, :] = supercell[0, :] + 2 * Dr
     supercell[1, :] = Ny * cell[1, :]
     supercell[2, :] = Nz * cell[2, :]
-    
-    
-    # Crystals 'a' and 'b' vectors and origin
-    crystal = np.zeros((3,3),dtype=float)
-    crystal[:,0] = 0.5 * supercell[0] 
-    crystal[:,1:3] = supercell[1:3].T  
 
-    crystals = [{},{}]
+    # Crystals 'a' and 'b' vectors and origin
+    crystal = np.zeros((3, 3), dtype=float)
+    crystal[:, 0] = 0.5 * supercell[0]
+    crystal[:, 1:3] = supercell[1:3].T
+
+    crystals = [{}, {}]
 
     for item in crystals:
-        item.update( {'crystal': crystal})
-        item.update( {'origin': np.zeros((3,1))}) 
+        item.update({'crystal': crystal})
+        item.update({'origin': np.zeros((3, 1))})
         #  Don't really understand how these work
         #         item.update( {'cs_idx': 0})
         #         item.update( {'cs_orientation': R})
         #         item.update( {'cs_origin': [0.0,0.0]})
-    
+
     crystals[1]['origin'] = 0.5 * supercell[:, 0:1]
-
-
 
     # Populate arrays of atom positions and types for a grain boundary supercell
     natoms_t = Nt * natoms
     species_gb_x = np.zeros(natoms_t, dtype=int)
     pos_gb_a = np.zeros((natoms_t, 3), dtype=float)
     pos_gb_b = np.zeros((natoms_t, 3), dtype=float)
-    crystals_idx = np.concatenate((np.zeros(natoms_t), np.ones(natoms_t))) # atom belonging to crystal index
+    # atom belonging to crystal index
+    crystals_idx = np.concatenate((np.zeros(natoms_t), np.ones(natoms_t)))
 
     # Populate arrays with atom positions
     a = 0
@@ -186,53 +185,54 @@ def construct_180_001_mZrO2(cellfile, repeats=[3, 1, 1], bound_vac=0.2,
         'supercell': supercell.T,
         'pos_gb_a': pos_gb_a,
         'pos_gb_b': pos_gb_b,
-        'atom_sites' : np.concatenate((pos_gb_a, pos_gb_b)).T,
-        'crystals' : crystals,
-        'crystals_idx' : crystals_idx,
+        'atom_sites': np.concatenate((pos_gb_a, pos_gb_b)).T,
+        'crystals': crystals,
+        'crystals_idx': crystals_idx,
         'species_gb_x': species_gb_x,
-        'all_species' : np.array(species_key),
-        'all_species_idx' : np.concatenate((species_gb_x, species_gb_x))
+        'all_species': np.array(species_key),
+        'all_species_idx': np.concatenate((species_gb_x, species_gb_x))
     }
 
     return out
 
 
 # Helper functions
-def  term_h00(pos_f, plane='100'):
+def term_h00(pos_f, plane='100'):
     """
     Translates and wraps fractional coordinates of m-ZrO2 primitive unit cell 
     for (h00) termination planes.
-    
+
     Notes
     ----
     Only works for fractional coordinates and primitive unit cell for now.
-    
+
     """
     if plane == '200' or plane == '-200':
-        tvecs = np.array([1 / int(plane[0]), 0, 0]) # translation vector
+        tvecs = np.array([1 / int(plane[0]), 0, 0])  # translation vector
         pos_f_t = pos_f + tvecs
         pos_f_t = wrap(pos_f_t)
-        
+
         return pos_f_t
 
     elif plane == '100':
         pos_f_t = pos_f
 
         return pos_f_t
-        
+
     else:
-        raise ValueError ('Specify a valid (h00) termination plane: `100` or `200`')
+        raise ValueError(
+            'Specify a valid (h00) termination plane: `100` or `200`')
 
 
 def get_species_key_idx(species_str):
     """
     Create a species key and list of species indices based on that key.
-    
+
     Parameters
     ----------
     species_str : list of str
         A list of strings for the atomic species symbols.
-        
+
     Returns
     -------
     species_key : list of str
@@ -240,22 +240,23 @@ def get_species_key_idx(species_str):
     species_idx : list of str
         List of length n, containing indices according to `species_key` for
         each atom as ordered in `species_str `.
-    
+
     """
 
     # Species convert string to number according to dictionary
-    species_key = reduce(lambda l, x: l if x in l else l+[x], species_str, [])
+    species_key = reduce(
+        lambda l, x: l if x in l else l + [x], species_str, [])
     species_idx = np.zeros([len(species_str)])
     for i, atom in enumerate(species_str):
         species_idx[i] = species_key.index(atom)
-        
+
     return species_key, species_idx
 
 
 def align_mon_cart(cell_vecs, axis='c', pos_a=None):
     """
     Align monoclinic ZrO2 with Cartesian reference frame.
-    
+
     Parameters
     ----------
     cell_vecs : ndarray
@@ -278,16 +279,16 @@ def align_mon_cart(cell_vecs, axis='c', pos_a=None):
     """
 
     if axis == 'c':
-        d = np.arctan2(cell_vecs[2,2], cell_vecs[2,0]) - np.pi/2
-        R_c = rotation_matrix(np.array([0,1,0]),d).squeeze()
+        d = np.arctan2(cell_vecs[2, 2], cell_vecs[2, 0]) - np.pi / 2
+        R_c = rotation_matrix(np.array([0, 1, 0]), d).squeeze()
         cell_r = np.dot(R_c, cell_vecs.T).T
 
     if pos_a is not None:
-        pos_a_r = np.transpose(np.dot(R_c,np.transpose(pos_a)))
+        pos_a_r = np.transpose(np.dot(R_c, np.transpose(pos_a)))
         return cell_r, pos_a_r
     else:
         return cell_r
-    
+
 
 def frac2abs(cell_vecs, pos_f):
     """
@@ -306,22 +307,21 @@ def frac2abs(cell_vecs, pos_f):
         Array of shape (n, 3), where the row vectors are the absolute
         coordinates of the atoms and n is the number of atoms.
     """
-    
-    
-    pos_a = np.zeros((pos_f.shape[0],3),dtype=float)   # Number of atoms = pos_f_rows
-    
+
+    # Number of atoms = pos_f_rows
+    pos_a = np.zeros((pos_f.shape[0], 3), dtype=float)
+
     for i in range(pos_f.shape[0]):
         for j in range(3):
-            pos_a[i,:] = pos_a[i,:] + pos_f[i,j] * cell_vecs[j,:]
+            pos_a[i, :] = pos_a[i, :] + pos_f[i, j] * cell_vecs[j, :]
 
     return pos_a
 
 
-def zero_prec(A, tol = 1e-12):
+def zero_prec(A, tol=1e-12):
     """
     Sets elements of an array within some tolerance `tol` to 0.0.
 
     """
     A[abs(A) < tol] = 0.0
     return A
-

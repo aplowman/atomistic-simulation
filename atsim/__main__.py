a6dc47c446383f4f8e42a1de59093815a7dd7d93
APlowman <adam.jp@live.co.uk>
2017-10-07 14:19:10 +0100

    Refactor

--- atsim/__main__.py
+++ atsim/__main__.py
@@ -3,13 +3,15 @@ import os
 import yaml
 from atsim import readwrite
 from atsim import options_parser
+from atsim import OPT_FILE_NAMES
 
 args = sys.argv
+opt_lkup_fn = OPT_FILE_NAMES['lookup']
+opt_def_fn = OPT_FILE_NAMES['defaults']
 
 if len(args) == 1 or args[1] == 'make':
     from atsim.simulation import makesims
-    opt_fn = 'makesims_opt.yml'
-    opt_lkup_fn = 'makesims_opt_lookup.yml'
+    opt_fn = OPT_FILE_NAMES['makesims']
     ms_opt = options_parser.validate_ms_opt(opt_fn, opt_lkup_fn)
     makesims.main(ms_opt)
 
@@ -17,8 +19,7 @@ elif args[1] == 'process':
     if len(args) != 3:
         raise ValueError('Specify SID to process.')
     from atsim.analysis import process
-    opt_fn = 'process_opt.yml'
-    opt_lkup_fn = 'makesims_opt_lookup.yml'
+    opt_fn = OPT_FILE_NAMES['process']
     ps_opt = options_parser.validate_ps_opt(opt_fn, opt_lkup_fn)
     process.main(ps_opt, args[2])
 
@@ -30,25 +31,16 @@ elif args[1] == 'submit_process':
 
 elif args[1] == 'harvest':
     from atsim.analysis import harvest
-    opt_fn = 'harvest.yml'
-    opt_lkup_fn = 'makesims_opt_lookup.yml'
-    opt_def_fn = 'defaults.yml'
+    opt_fn = OPT_FILE_NAMES['harvest']
     hv_opt = options_parser.validate_hv_opt(opt_fn, opt_lkup_fn, opt_def_fn)
-
-    print('hv_opt: {}'.format(hv_opt))
-
     harvest.main(hv_opt)
 
 elif args[1] == 'plot':
-    if len(args) != 3:
-        raise ValueError('Specify RID for plotting data source.')    
-    from atsim.analysis import makeplots    
-    opt_fn = 'makeplots.yml'
-    opt_lkup_fn = 'makesims_opt_lookup.yml'
-    opt_def_fn = 'defaults.yml'
+    from atsim.analysis import makeplots
+    opt_fn = OPT_FILE_NAMES['makeplots']
     mp_opt = options_parser.validate_mp_opt(opt_fn, opt_lkup_fn, opt_def_fn)
-    makeplots.main(args[2], mp_opt)
-    
+    makeplots.main(mp_opt)
+
 elif args[1] == 'series_helper':
     if len(args) != 3:
         raise ValueError('Specify SID to series_helper.')

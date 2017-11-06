82b225815fd939d75f7ade3af688ae0f874aa13d
APlowman <adam.jp@live.co.uk>
2017-11-06 11:18:19 +0000

    Regenerate a Bicrystal if importing from a Bicrystal

--- atsim/simulation/sim.py
+++ atsim/simulation/sim.py
@@ -4,6 +4,7 @@ from plotly import graph_objs
 from plotly.offline import plot, iplot, init_notebook_mode
 from atsim import plotting, geometry, vectors, mathsutils, readwrite, utils
 from atsim.structure.atomistic import AtomisticStructure
+from atsim.structure.bicrystal import Bicrystal
 from atsim.structure.crystal import CrystalBox, CrystalStructure
 from atsim.simsio import castep, lammps
 
@@ -20,7 +21,7 @@ class AtomisticSimulation(object):
 
     def generate_structure(self, opt_idx=-1, tile=None):
         """
-        Generates an AtomisticStructure object for a given optimisation step 
+        Generate an AtomisticStructure object for a given optimisation step
         after the simulation has run.
 
         Parameters
@@ -30,14 +31,10 @@ class AtomisticSimulation(object):
 
         tile : tuple of int, optional
             The integer number of times to tile the supercell in each
-            supercell direction. Default is None, in which cas the supercell 
+            supercell direction. Default is None, in which cas the supercell
             is not tiled.
 
         """
-
-        # TODO: consider reordering of atoms (in e.g. CASTEP) when generating
-        # new structure. I.e. generating a new AS object from opt_step 0 should
-        # write identical input files to the originals.
 
         if self.results is None:
             raise ValueError('Cannot generate an optimised AtomisticStructure '
@@ -49,6 +46,8 @@ class AtomisticSimulation(object):
 
         species_orig = self.structure.all_species
         species_idx_orig = self.structure.all_species_idx
+        supercell_orig = self.structure.supercell
+        supercell_orig_inv = np.linalg.inv(supercell_orig)
 
         if method == 'castep':
 
@@ -59,30 +58,64 @@ class AtomisticSimulation(object):
                 species_orig, species_idx_orig)
             reorder_map_inv = np.argsort(reorder_map)
 
-            # atoms reordered based on original species_idx
+            # Atoms reordered based on original species_idx
             atom_sites = atom_sites[:, reorder_map_inv]
-
-            # cell_constraints = self.options['constraints']['cell']
-            # prt(cell_constraints, 'cell_constraints')
-
-            # if (cell_constraints['fix_lengths'] == 'abc' and
-            #         cell_constraints['fix_angles'] == 'abc'):
-
-            #     # supercell remains fixed, we can include crystal definitions:
-            #     crystals = self.structure.crystals
-            #     crystal_idx = self.structure.crystal_idx
-            #     prt(crystals, 'crystals')
-            #     prt(crystal_idx, 'crystal_idx')
-            #     exit()
 
         elif method == 'lammps':
 
             supercell = self.results['supercell'][opt_idx]
             atom_sites = self.results['atoms'][opt_idx]
 
-        opt_structure = AtomisticStructure(
-            atom_sites, supercell, all_species=species_orig,
-            all_species_idx=species_idx_orig)
+        # Assume the bounds of each crystal to remain at fixed fractional
+        # coordinates within the supercell during relaxation.
+
+        crystals_new = []
+        for c_idx, c in enumerate(self.structure.crystals):
+
+            c_vecs = c['crystal']
+            c_origin = c['origin']
+
+            # Transform to fractional coordinates in the original supercell
+            c_vecs_sup = np.dot(supercell_orig_inv, c_vecs)
+            c_origin_sup = np.dot(supercell_orig_inv, c_origin)
+
+            # Transform to Cartesian coordinates in the new supercell
+            c_vecs_new = np.dot(supercell, c_vecs_sup)
+            c_origin_new = np.dot(supercell, c_origin_sup)
+
+            crystals_new.append({
+                'crystal': c_vecs_new,
+                'origin': c_origin_new,
+            })
+
+            additional_info = ['cs_idx', 'cs_orientation', 'cs_origin']
+            for i in additional_info:
+                if c.get(i) is not None:
+                    crystals_new[-1].update({i: c[i]})
+
+        as_params = {
+            'atom_sites': atom_sites,
+            'supercell': supercell,
+            'all_species': species_orig,
+            'all_species_idx': species_idx_orig,
+            'crystals': self.structure.crystals,
+            'crystal_idx': self.structure.crystal_idx,
+            'crystal_structures': self.structure.crystal_structures,
+        }
+
+        if 'bicrystal' in self.structure.meta:
+
+            # Regenerate a bicrystal
+            bc_params = {
+                'as_params': as_params,
+                'maintain_inv_sym': self.structure.maintain_inv_sym,
+                'nbi': self.structure.nbi,
+                'rot_mat': self.structure.rot_mat
+            }
+            opt_structure = Bicrystal(**bc_params)
+
+        else:
+            opt_structure = AtomisticStructure(**as_params)
 
         if tile is not None:
             opt_structure.tile_supercell(tile)

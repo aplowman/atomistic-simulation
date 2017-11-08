38ac57eb5b3b71720fb9b1d84d1ed03a5ccc01ac
APlowman <adam.jp@live.co.uk>
2017-11-08 16:29:55 +0000

    Add plot_geometry_plotly

--- atsim/plotting.py
+++ atsim/plotting.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import copy
 from atsim import utils
+import geometry
 
 # Plotly
 from plotly import tools
@@ -1501,3 +1502,353 @@ def add_contour_trace_mpl(fig, ax, x, y, z, name, options=None):
 
     cbar = fig.colorbar(cset, ax=ax)
     cbar.set_label(name)
+
+
+def set_dict_def(d_default, d=None):
+    """ TODO: move to utils """
+    if d is None:
+        d = d_default
+    else:
+        d = {**d_default, **d}
+    return d
+
+
+def plot_geometry_plotly(points=None, boxes=None, style=None, plot_3d=True, plot_2d='xyz'):
+    """
+
+    Next time:
+        factorise logic to get subplot sizes out so can share between plotly and mpl function
+
+
+    """
+
+    STYLE_DEF = {
+        'width': 700,
+        'height': 700,
+        'aspect': 'equal',
+        'labels': ['x', 'y', 'z'],
+    }
+
+    COLS = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+            '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+
+    if points is None:
+        points = []
+    if boxes is None:
+        boxes = []
+
+    plot_dirs = []
+    if 'x' in plot_2d:
+        plot_dirs.append(0)
+    if 'y' in plot_2d:
+        plot_dirs.append(1)
+    if 'z' in plot_2d:
+        plot_dirs.append(2)
+
+    ax_lab_2d = (('x', 'y'), ('x2', 'y'), ('x', 'y3'))
+    show_leg_2d = (True, False, False)
+
+    if len(plot_dirs) == 3:
+        dirs_2d = ((1, 2), (0, 2), (1, 0))
+
+    elif len(plot_dirs) == 2:
+
+        if plot_dirs == [0, 1]:
+            dirs_2d = ((1, 2), (0, 2), None)
+            ax_lab_2d = (('x', 'y'), ('x2', 'y'), None)
+
+        if plot_dirs == [0, 2]:
+            dirs_2d = ((2, 1), None, (0, 1))
+            ax_lab_2d = (('x', 'y'), None, ('x2', 'y'))
+
+        if plot_dirs == [1, 2]:
+            dirs_2d = (None, (2, 0), (1, 0))
+            ax_lab_2d = (None, ('x', 'y'), ('x2', 'y'))
+
+    elif len(plot_dirs) == 1:
+
+        if plot_dirs == [0]:
+            dirs_2d = ((1, 2), None, None)
+            ax_lab_2d = (('x', 'y'), None, None)
+
+        if plot_dirs == [1]:
+            dirs_2d = (None, (0, 2), None)
+            ax_lab_2d = (None, ('x', 'y'), None)
+
+        if plot_dirs == [2]:
+            dirs_2d = (None, None, (0, 1),)
+            ax_lab_2d = (None, None, ('x', 'y'))
+
+    # print('plot_dirs: {}'.format(plot_dirs))
+    # print('dirs_2d: {}'.format(dirs_2d))
+    # print('ax_lab_2d: {}'.format(ax_lab_2d))
+
+    style = set_dict_def(STYLE_DEF, style)
+
+    data_3d = []
+    data_2d = []
+
+    layout_3d = {
+        'width': style['width'],
+        'height': style['height'],
+    }
+    scene = {
+        'xaxis': {
+            'title': style['labels'][0]
+        },
+        'yaxis': {
+            'title': style['labels'][1]
+        },
+        'zaxis': {
+            'title': style['labels'][2]
+        }
+    }
+    if style['aspect'] == 'equal':
+        scene.update({
+            'aspectmode': 'data'
+        })
+    layout_3d.update({'scene': scene})
+
+    for pts in points:
+        d = {
+            'type': 'scatter3d',
+            'mode': 'markers',
+            'marker': {
+                'color': pts['colour'],
+                'symbol': pts['symbol'],
+                # Crosses are unusually large:
+                'size': 5 if pts['symbol'] == 'x' else 8,
+            },
+            'x': pts['data'][0],
+            'y': pts['data'][1],
+            'z': pts['data'][2],
+        }
+        if pts.get('name') is not None:
+            d.update({'name': pts.get('name')})
+        data_3d.append(d)
+
+        for i in plot_dirs:
+            # print('i: {}'.format(i))
+            # print('ax_lab_2d[i]: {}'.format(ax_lab_2d[i]))
+            # print('dirs_2d[i]: {}'.format(dirs_2d[i]))
+
+            d = {
+                'type': 'scatter',
+                'mode': 'markers',
+                'marker': {
+                    'color': pts['colour'],
+                    'symbol': pts['symbol'],
+                },
+                'x': pts['data'][dirs_2d[i][0]],
+                'y': pts['data'][dirs_2d[i][1]],
+                'xaxis': ax_lab_2d[i][0],
+                'yaxis': ax_lab_2d[i][1],
+                'showlegend': show_leg_2d[i],
+            }
+            if pts.get('name') is not None:
+                d.update(
+                    {
+                        'name': pts.get('name'),
+                        'legendgroup': pts.get('name'),
+                    }
+                )
+            data_2d.append(d)
+
+    for bx_idx, bx in enumerate(boxes):
+
+        bx_def = {
+            'colour': COLS[bx_idx],
+            'origin': np.array([0, 0, 0]),
+        }
+        bx = set_dict_def(bx_def, bx)
+
+        bx_trace = geometry.get_box_xyz(bx['edges'], origin=bx['origin'])[0]
+        d = {
+            'type': 'scatter3d',
+            'mode': 'lines',
+            'x': bx_trace[0],
+            'y': bx_trace[1],
+            'z': bx_trace[2],
+            'line': {
+                'color': bx['colour']
+            }
+        }
+        if bx.get('name') is not None:
+            d.update({'name': bx.get('name')})
+        data_3d.append(d)
+
+        for i in plot_dirs:
+
+            d = {
+                'type': 'scatter',
+                'mode': 'lines',
+                'line': {
+                    'color': bx['colour'],
+                },
+                'x': bx_trace[dirs_2d[i][0]],
+                'y': bx_trace[dirs_2d[i][1]],
+                'xaxis': ax_lab_2d[i][0],
+                'yaxis': ax_lab_2d[i][1],
+                'showlegend': show_leg_2d[i],
+            }
+            if bx.get('name') is not None:
+                d.update(
+                    {
+                        'name': bx.get('name'),
+                        'legendgroup': bx.get('name'),
+                    }
+                )
+            data_2d.append(d)
+
+    # 2D projections layout
+    # =====================
+
+    hori_space = 0.05
+    vert_space = 0.05
+
+    # Get min and max data in plots and boxes:
+    all_x = []
+    all_y = []
+    all_z = []
+    for d in data_3d:
+        all_x.extend(d['x'])
+        all_y.extend(d['y'])
+        all_z.extend(d['z'])
+
+    min_x, min_y, min_z = [np.min(i) for i in [all_x, all_y, all_z]]
+    max_x, max_y, max_z = [np.max(i) for i in [all_x, all_y, all_z]]
+
+    x_rn = max_x - min_x
+    y_rn = max_y - min_y
+    z_rn = max_z - min_z
+
+    y_frac = 1
+    y3_frac = 0
+    x_frac = 1
+    x2_frac = 0
+
+    if len(plot_dirs) == 3:
+        tot_rn_vert = z_rn + x_rn
+        tot_rn_hori = y_rn + x_rn
+
+        y_frac = z_rn / tot_rn_vert
+        y3_frac = x_rn / tot_rn_vert
+        x_frac = y_rn / tot_rn_hori
+        x2_frac = x_rn / tot_rn_hori
+
+    elif len(plot_dirs) == 2:
+
+        vert_space = 0
+
+        if plot_dirs == [0, 1]:
+            tot_rn_vert = z_rn
+            tot_rn_hori = y_rn + x_rn
+            x_frac = y_rn / tot_rn_hori
+            x2_frac = x_rn / tot_rn_hori
+
+        elif plot_dirs == [0, 2]:
+            tot_rn_vert = y_rn
+            tot_rn_hori = z_rn + x_rn
+            x_frac = z_rn / tot_rn_hori
+            x2_frac = x_rn / tot_rn_hori
+
+        elif plot_dirs == [1, 2]:
+            tot_rn_vert = x_rn
+            tot_rn_hori = z_rn + y_rn
+            x_frac = z_rn / tot_rn_hori
+            x2_frac = y_rn / tot_rn_hori
+
+    elif len(plot_dirs) == 1:
+
+        hori_space = 0
+        vert_space = 0
+
+    xaxis1 = {
+        'domain': [0, x_frac - hori_space / 2],
+        'anchor': 'y',
+    }
+    yaxis1 = {
+        'domain': [y3_frac + vert_space / 2, 1],
+        'anchor': 'x',
+        'scaleanchor': 'x',
+    }
+    xaxis2 = {
+        'domain': [x_frac + hori_space / 2, 1],
+        'anchor': 'y',
+        'scaleanchor': 'y',
+    }
+    yaxis3 = {
+        'domain': [0, y3_frac - vert_space / 2],
+        'anchor': 'x',
+        'scaleanchor': 'x',
+    }
+
+    if len(plot_dirs) == 3:
+        xaxis1.update({
+            'title': 'y',
+            'side': 'top',
+        })
+        yaxis1.update({
+            'title': 'z',
+        })
+        xaxis2.update({
+            'title': 'x',
+            'side': 'top',
+        })
+        yaxis3.update({
+            'title': 'x',
+        })
+
+    elif len(plot_dirs) == 2:
+
+        if plot_dirs == [0, 1]:
+            xaxis1.update({'title': 'y', })
+            yaxis1.update({'title': 'z', })
+            xaxis2.update({
+                'title': 'x',
+            })
+
+        elif plot_dirs == [0, 2]:
+            xaxis1.update({'title': 'z', })
+            yaxis1.update({'title': 'y', })
+            xaxis2.update({
+                'title': 'x',
+            })
+
+        elif plot_dirs == [1, 2]:
+            xaxis1.update({'title': 'z', })
+            yaxis1.update({'title': 'x', })
+            xaxis2.update({
+                'title': 'y',
+            })
+
+    elif len(plot_dirs) == 1:
+
+        if plot_dirs == [0]:
+            xaxis1.update({'title': 'y', })
+            yaxis1.update({'title': 'z', })
+
+        elif plot_dirs == [1]:
+            xaxis1.update({'title': 'x', })
+            yaxis1.update({'title': 'z', })
+
+        elif plot_dirs == [2]:
+            xaxis1.update({'title': 'x', })
+            yaxis1.update({'title': 'y', })
+
+    layout_2d = {
+        'width': style['width'],
+        'height': style['width'],
+        'xaxis1': xaxis1,
+        'yaxis1': yaxis1,
+    }
+
+    if len(plot_dirs) == 3:
+        layout_2d.update({'yaxis3': yaxis3, })
+    if len(plot_dirs) >= 2:
+        layout_2d.update({'xaxis2': xaxis2, })
+
+    fig_2d = go.Figure(data=data_2d, layout=layout_2d)
+    fig_3d = go.Figure(data=data_3d, layout=layout_3d)
+
+    return (fig_3d, fig_2d)

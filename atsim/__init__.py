c62d30d7ff229f4a1d450e44348d1787177debf5
APlowman <adam.jp@live.co.uk>
2017-11-11 14:32:45 +0000

    Modify options file names temporarily whilst working on new branch

--- atsim/__init__.py
+++ atsim/__init__.py
@@ -33,11 +33,11 @@ SERIES_NAMES = list(ALLOWED_SERIES_KEYS.keys())
 
 # Options file names
 OPT_FILE_NAMES = {
-    'makesims': 'makesims.yml',
+    'makesims': 'makesims_old.yml',
     'process': 'process.yml',
     'harvest': 'harvest.yml',
     'makeplots': 'makeplots.yml',
     'serieshelper': 'serieshelper.yml',
-    'lookup': 'lookup.yml',
+    'lookup': 'lookup_old.yml',
     'defaults': 'defaults.yml',
 }

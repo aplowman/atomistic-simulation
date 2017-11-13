9595f43f8d4f862752426844ad3253189cffc6f1
APlowman <adam.jp@live.co.uk>
2017-11-13 10:13:53 +0000

    Add check_indices

--- atsim/utils.py
+++ atsim/utils.py
@@ -837,3 +837,29 @@ def combination_idx(*seq):
         ret_idx.append(np.where(uinv == i)[0])
 
     return ret_k, ret_idx
+
+
+def check_indices(seq, seq_idx):
+    """
+    Given a sequence (e.g. list, tuple, ndarray) which is indexed by another,
+    check the indices are sensible.
+
+    Parameters
+    ----------
+    seq : sequence
+    seq_idx : sequence of int
+
+    """
+
+    # Check: minimum index is greater than zero
+    if min(seq_idx) < 0:
+        raise IndexError('Found index < 0.')
+
+    # Check maximum index is equal to length of sequence - 1
+    if max(seq_idx) > len(seq) - 1:
+        raise IndexError('Found index larger than seqence length.')
+
+
+def to_col_vec(a, dim=3):
+    a = np.array(a).squeeze().reshape((dim, 1))
+    return a

1d499f883b601f94e7beb68b8e994969bfa20f8c
APlowman <adam.jp@live.co.uk>
2017-12-26 15:57:23 +0000

    Add TODO to setup

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
+"""Pip installation script.
+
+TODO:
+    - On installation, ask where to put option file directory (with default)
+"""
 from setuptools import find_packages, setup
 
-setup(name='atsim',
-      version="0.1",
-      description="Python tools for setting up and analysing the results from CASTEP and LAMMPS simulations.",
-      author='APlowman',
-      packages=find_packages(),
-      install_requires=[
-          'plotly',
-          'spglib',
-          'numpy',
-          'matplotlib',
-          'mendeleev',
-          'dropbox',
-          'PyYAML',
-      ],
-      entry_points={
-          'console_scripts': []
-      }
-      )
+setup(
+    name='atsim',
+    version="0.1",
+    description=(
+        "Python tools for setting up and analysing the results from CASTEP "
+        "and LAMMPS simulations."
+    ),
+    author='Adam J. Plowman, Maria S. Yankova',
+    packages=find_packages(),
+    install_requires=[
+        'plotly',
+        'spglib',
+        'numpy',
+        'matplotlib',
+        'mendeleev',
+        'dropbox',
+        'PyYAML',
+        'beautifultable',
+    ]
+)

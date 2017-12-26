c9e1ae676de654ac576e029fe5690c0b7cbb9701
APlowman <adam.jp@live.co.uk>
2017-12-26 16:57:13 +0000

    Remove unused import

--- atsim/plotting.py
+++ atsim/plotting.py
@@ -13,9 +13,6 @@ from plotly import graph_objs as go
 # Matplotlib
 import matplotlib.pyplot as plt
 
-# Bokeh
-import bokeh.plotting as bok_plot
-
 
 COLS = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
         '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']

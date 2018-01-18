b63b92bac4549087d638969ba9c39144158f5603
APlowman <adam.jp@live.co.uk>
2018-01-18 08:55:31 +0000

    Add functions to convert site labels to/from JSONable dicts

--- atsim/structure/__init__.py
+++ atsim/structure/__init__.py
@@ -1,4 +1,34 @@
 import os
 from pathlib import Path
+import numpy as np
 
 REF_PATH = os.path.join(str(Path(__file__).parents[1]), 'ref')
+
+
+def site_labs_to_jsonable(labs):
+    """Get a representation of a site labels dict which can be JSON
+    serialised.
+
+    Site labels have the form dict of {str: tuple of ndarrays}
+
+    """
+    if labs is None:
+        return
+    labs_js = {}
+    for lab_name, lab_val in labs.items():
+        labs_js.update({
+            lab_name: [np.array(i).tolist() for i in lab_val]
+        })
+    return labs_js
+
+
+def site_labs_from_jsonable(labs):
+    """Convert site labels from JSONable form to desired form."""
+    if labs is None:
+        return
+    labs_native = {}
+    for lab_name, lab_val in labs.items():
+        labs_native.update({
+            lab_name: tuple([np.array(i) for i in lab_val])
+        })
+    return labs_native
